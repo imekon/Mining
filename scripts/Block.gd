39202778b27a6d78bdec3642f677a8101b0d3e8a
Pete Goodwin <imekon@users.noreply.github.com>
2020-04-11 11:58:14 +0100

    masking and stuff

--- scripts/Block.gd
+++ scripts/Block.gd
@@ -2,9 +2,15 @@ extends Node
 
 class_name Block
 
+const tile_health = [ 0, 0, 2, 3, 5, 8 ]
+const tile_score = [ 0, 0, 5, 10, 20, 50 ]
+
 var tile
 var health
 
 func _init():
 	tile = -1
 	health = -1
+
+func setup():
+	health = tile_health[tile]
--- scripts/Main.gd
+++ scripts/Main.gd
@@ -6,29 +6,87 @@ const MINE_CELL_SIZE = 32
 
 onready var tilemap = $TileMap
 onready var player = $Player
+onready var score_label = $CanvasLayer/ScoreLabel
 
 var player_x
 var player_y
+var player_score
+
 var mine = []
+
+# Tiles
+# 0 - barrier, edge of the world
+# 1 - empty
+# 2-4 - rocks
+# 5 - coal
 
 func _ready():
 	randomize()
 	player_x = randi() % (MINE_WIDTH - 2) + 1
 	player_y = 1
+	player_score = 0
 	set_player_position()
 	build_mine()
-
+	
+func _process(delta):
+	score_label.text = 'Score: ' + str(player_score)
+	
+	var dx = 0
+	var dy = 0
+	
+	if Input.is_action_just_pressed("left"):
+		dx = -1
+	if Input.is_action_just_pressed("right"):
+		dx = 1
+	if Input.is_action_just_pressed("up"):
+		dy = -1
+	if Input.is_action_just_pressed("down"):
+		dy = 1
+		
+	try_move_player(dx, dy)
+	
+func try_move_player(dx, dy):
+	var block = mine[player_y + dy][player_x + dx]
+	if block == null:
+		move_player(dx, dy)
+		return
+		
+	if block.tile == 0:
+		return
+	
+	block.health -= 1
+	
+	if block.health <= 0:
+		destroy_block(player_x + dx, player_y + dy, block)
+		move_player(dx, dy)
+		
+func move_player(dx, dy):
+	player_x += dx
+	player_y += dy
+	set_player_position()
+	
 func set_player_position():
 	player.position.x = player_x * MINE_CELL_SIZE
 	player.position.y = player_y * MINE_CELL_SIZE
+	
+func destroy_block(x, y, block):
+	player_score += Block.tile_score[block.tile]
+	mine[y][x] = null
+	tilemap.set_cell(x, y, 1)
+	block.queue_free()
+	pass
 	
 func build_mine():
 	for y in range(0, MINE_HEIGHT):
 		var row = []
 		for x in range(0, MINE_WIDTH):
 			var block = Block.new()
-			block.tile = randi() % 3 + 1
-			block.health = 4
+			var tile = randi() % 3 + 2
+			var percent = randi() % 100
+			if percent > 90:
+				tile = 5
+			block.tile = tile
+			block.setup()
 			row.append(block)
 		mine.append(row)
 		
